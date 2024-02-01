196e8934c9572cb65aabe87bccec1c56d2dd1231
DESKTOP-SCD7G66\Raul <coroiuraul@yahoo.com>
2024-02-01 13:44:12 +0200

    update user functions and handlers

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,1 +1,54 @@
 package user
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	ErrorFailedToFetchRecord = "failed to fetch record"
+)
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName`
+}
+
+func FetchUser(email, tablename string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tablename),
+	}
+
+	restult, err := dynaClient.GetItem(input)
+
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() error {
+
+}
